595c25da28c0b7de11d33e25dfef6fec5af4d928
MilkyWay-WDS <dgmilkyway@wds.co>
2014-12-02 16:56:59 +0200

    TS [GMKT-212] Fix test running on jenkins

--- test/usermgmt/AbstractTest.java
+++ test/usermgmt/AbstractTest.java
@@ -2,83 +2,31 @@ package usermgmt;
 
 import static play.test.Helpers.*;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.junit.After;
 import org.junit.Before;
 
-import com.avaje.ebean.Ebean;
-
 import play.test.FakeApplication;
-import play.test.TestServer;
-import usermgmt.models.AbstractModel;
-import usermgmt.models.User;
 import usermgmt.utils.AdditionalConfiguration;
 
 
 public abstract class AbstractTest {
-
-	private static final int DEFAULT_PORT = 3333;
 	
-	private TestServer server;
+	private FakeApplication fakeApplication;
 	
 	@Before
 	public void setUp(){
-		server = createTestServer();
-		start(server);
-		cleanDatabase(User.class);
+		fakeApplication = createFakeApplication();
+		start(fakeApplication);
 	}
 	
 	@After
 	public void tearDown(){
-		stop(server);
+		stop(fakeApplication);
 	}
 	
-	private static TestServer createTestServer(){
-		FakeApplication fakeApplication = fakeApplication(getUsermgmtDbParams());
-		TestServer result = testServer(DEFAULT_PORT, fakeApplication);
-		return result;
+	protected FakeApplication createFakeApplication(){
+		return fakeApplication(inMemoryDatabase(AdditionalConfiguration.EBEAN_SERVER.getValue()));
 	}
 	
-	private static Map<String, String> getUsermgmtDbParams(){
-		Map<String, String> result = new HashMap<String, String>();
-        result.put("db.usermgmt.driver", "com.mysql.jdbc.Driver");
-        result.put("db.usermgmt.url", "jdbc:mysql://localhost:3306/usermgmt?characterEncoding=UTF-8");
-        result.put("db.usermgmt.user", "test");
-        result.put("db.usermgmt.password", "test");
-        result.put("ebean.usermgmt", "usermgmt.models.*");
-        result.put("applyEvolutions.default", "true");
-
-        return result;
-	}
-	
-	@SafeVarargs
-	private static void cleanDatabase(Class<? extends AbstractModel>... entitiesTypes){
-		for (Class<? extends AbstractModel> classValue : entitiesTypes) {
-            clearTable(classValue);
-        }
-	}
-	
-	private static void clearTable(Class<? extends AbstractModel> classValue) {
-        clearEntities(classValue);
-        alterTable(classValue);
-    }
-	
-	private static void clearEntities(Class<? extends AbstractModel> classValue){
-		Iterable<? extends AbstractModel> models = Ebean.getServer(AdditionalConfiguration.EBEAN_SERVER.getValue()).find(classValue).findList();
-        for (AbstractModel model : models) {
-        	Ebean.getServer(AdditionalConfiguration.EBEAN_SERVER.getValue()).delete(model);
-        }
-	}
-	
-	private static void alterTable(Class<? extends AbstractModel> classValue){
-		String tableName = classValue.getSimpleName().toLowerCase();
-		alterTable(tableName);
-	}
-	
-	private static void alterTable(String tableName){
-		Ebean.getServer(AdditionalConfiguration.EBEAN_SERVER.getValue()).createSqlUpdate(String.format("ALTER TABLE %s AUTO_INCREMENT = 1", tableName)).execute();
-	}
 	
 }
