3a0179d5faded4f165af4be965a8040c590ea8e0
MilkyWay[Kyle] <wds-kwl@globallogic.com>
2014-12-12 13:07:42 +0200

    YK/IuS [GMKT-235] Add config for tests

--- test/views/usermgmt/TestHelper.java
+++ test/views/usermgmt/TestHelper.java
@@ -1,13 +1,22 @@
 package views.usermgmt;
 
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
+import play.Configuration;
 import play.test.FakeApplication;
 import utils.usermgmt.AdditionalConfiguration;
+
+import java.io.File;
+import java.util.Map;
 
 import static play.test.Helpers.fakeApplication;
 import static play.test.Helpers.inMemoryDatabase;
 
 public class TestHelper {
     public static FakeApplication createFakeApplication() {
-        return fakeApplication(inMemoryDatabase(AdditionalConfiguration.EBEAN_SERVER.getValue()));
+        Config additionalConfig = ConfigFactory.parseFile(new File("conf/usermgmt.test.conf"));
+        Map<String, Object> additionalConfigurations = new Configuration(additionalConfig).asMap();
+        additionalConfigurations.putAll(inMemoryDatabase(AdditionalConfiguration.EBEAN_SERVER.getValue()));
+        return fakeApplication(additionalConfigurations);
     }
 }
